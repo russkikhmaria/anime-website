c9a7b01fafe3afa639f306d59c2a90ad647216c6
Maria Russkikh <marijarusskikh@gmail.com>
2025-04-19 13:11:49 +0300

    Add files via upload

---
+++ js/bg-elements.js
@@ -0,0 +1,10 @@
+const bgElements = () => {
+  const elements = document.querySelectorAll('.set-bg');
+
+  elements.forEach((elem) => {
+    elem.style.backgroundImage = `url(${elem.dataset.setbg})`;
+  });
+
+};
+
+bgElements();
